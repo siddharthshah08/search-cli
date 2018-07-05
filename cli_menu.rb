f4306e8dfdbf9353ee0c36938fe3e452107558da
Siddharth Shah <siddharthshah@Sids-MacBook-Pro.local>
2018-07-05 09:50:36 -0700

    fix: pretty print json

--- cli_menu.rb
+++ cli_menu.rb
@@ -7,15 +7,38 @@ VALID_SEARCH_FIELDS = {
   '3' => %w[_id url external_id name domain_names created_at details shared_tickets tags]
 }.freeze
 OBJECT_MAP = { '1' => 'users', '2' => 'tickets', '3' => 'organizations' }.freeze
+QUIT_COMMAND = 'quit'.freeze
 require './models/search.rb'
 require 'json'
 require 'pp'
+
+
 def display_menu
   puts "\n"
   puts "\t Select search options:"
   puts "\t * Press 1 to search Zendesk"
   puts "\t * Press 2 to view a list of searchable fields"
   puts "\t * Type 'quit' to exit"
+end
+
+def resource_selection
+  return 'Select 1) Users or 2) Tickets or 3) Organization'
+end
+
+def invalid_selection_prompt
+  return 'Invalid option'
+end
+
+def search_field_prompt
+  return 'Enter search field'
+end
+
+def search_value_prompt
+  return 'Enter search value'
+end
+
+def no_results_alert
+  return 'No results found'
 end
 def cli_options
   run = true
@@ -24,33 +47,33 @@ def cli_options
     command = gets.chomp
     case command
     when '1'
-      puts 'Select 1) Users or 2) Tickets or 3) Organization'
+        puts resource_selection
         valid_option = false
         while !valid_option
           option = gets.chomp.to_s
-          if option == 'quit'
+          if option == QUIT_COMMAND
             valid_option = true
             run = false
           elsif VALID_SEARCH_OPTIONS.include? option
             valid_option = true
-            puts 'Enter search field'
+            puts search_field_prompt
             field = gets.chomp.to_s
             if VALID_SEARCH_FIELDS[option].include? field
-              puts 'Enter search value'
+              puts search_value_prompt
               value = gets.chomp
               search = Model::Search.new
               result = search.search(OBJECT_MAP[option], field, value)
               if result.empty?
-                puts 'No results found'
+                puts no_results_alert
                 display_menu
               else
-                pp result
+                puts JSON.pretty_generate(result)
                 display_menu
               end
             end
           else
-            puts 'Invalid option'
-            puts 'Select 1) Users or 2) Tickets or 3) Organization'
+            puts resource_selection
+            puts invalid_selection_prompt
           end
         end
     when '2'
@@ -63,7 +86,7 @@ def cli_options
         puts "\n"
       end
       display_menu
-    when 'quit'
+    when QUIT_COMMAND
       run = false
       return
     else
